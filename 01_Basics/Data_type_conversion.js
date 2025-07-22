ba0c79a7ba562501b5799df22e19b9d340bd083d
Hossain-kobir <129597836+Hossain-kobir@users.noreply.github.com>
2025-07-22 11:05:45 +0000

    Data type conversion of js

---
+++ 01_Basics/Data_type_conversion.js
@@ -0,0 +1,48 @@
+let score = "45"
+let score2 = "45abc"
+let score3 = null
+let score4 = undefined
+
+
+
+console.log(typeof score)
+console.log(typeof(score2))
+
+let ValueInNumber= Number(score)
+let ValueInNumber2= Number(score2)
+let ValueInNumber3= Number(score3)
+let ValueInNumber4= Number(score4)
+
+console.log("typeof score : ")
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+console.log("typeof score 2 : 2 ")
+console.log(typeof ValueInNumber2)
+console.log(ValueInNumber2)
+console.log("typeof score 3 : 3 ")
+console.log(typeof ValueInNumber3)
+console.log(ValueInNumber3)
+console.log("typeof score 4 : 4 ")
+console.log(typeof ValueInNumber4)
+console.log(ValueInNumber4)
+
+// Boolean Type
+console.log("Now Check The Boolean Type :")
+let isloggedin = true
+let isloggedin2 = "Hossain Kobir"
+
+let booleanloogedin=Number(isloggedin)
+let boleanloogedin2 = Boolean(isloggedin2) 
+
+// Here is typeof bolean 3
+let boleanloogedin3 = Number(isloggedin2) 
+
+console.log("Here is typeof bolean 1")
+console.log(typeof (booleanloogedin))
+console.log(booleanloogedin)
+console.log("Here is typeof bolean 2")
+console.log(typeof boleanloogedin2)
+console.log(boleanloogedin2)
+
+console.log("Here is typeof bolean 3")
+console.log(boleanloogedin3)
