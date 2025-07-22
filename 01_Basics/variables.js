6717d76dd3041a89332725af44c5cd8446c6351d
Hossain-kobir <129597836+Hossain-kobir@users.noreply.github.com>
2025-07-22 10:58:53 +0000

    variables Topic

---
+++ 01_Basics/variables.js
@@ -0,0 +1,17 @@
+const AccountId=12334
+
+let AccountName = "Hossain Kobir"
+var AccountMail = "hossainkobir37bd@gmail.com"
+AccountCity = "Dhaka"
+
+let AccountState;
+
+//AccountId=2// Not Allowed
+
+console.log(AccountId)
+
+AccountName="Kobir Hossain"
+AccountMail="@gmail.com"
+AccountCity="Dhanmondi"
+
+console.table([AccountId,AccountName,AccountMail,AccountCity,AccountState])
