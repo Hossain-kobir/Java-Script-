31f486addb3e6584dd16c4ee099aedd994adedc1
Hossain-kobir <129597836+Hossain-kobir@users.noreply.github.com>
2025-07-22 11:03:31 +0000

    Data types of js

---
+++ 01_Basics/Data_types.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all code as a newar version //
+
+// alert("hello")  we are using Node js , Not Browser
+// alert(3+3)
+
+
+let Name ="Hossain Kobir"
+let age  = 27
+
+let isloggedin   //here is type of will be show undefined
+
+let islogin2=null //here is type of will be show object
+
+console.log(typeof isloggedin)
+
+console.log(typeof(islogin2))
