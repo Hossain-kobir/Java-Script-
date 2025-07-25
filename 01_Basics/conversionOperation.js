693152248cdd9166d346254a2fc2275aaece41ab
Hossain-kobir <129597836+Hossain-kobir@users.noreply.github.com>
2025-07-25 16:29:05 +0000

    Conversion Operations js

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,19 @@
+console.log("2"+2)
+
+console.log(2 + '2'+3)
+
+console.log(+true)
+
+let counter =100
+counter++
+
+console.log(counter)
+
+console.log(+"")
+
+console.log(+ null)
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+console.log(num1)
