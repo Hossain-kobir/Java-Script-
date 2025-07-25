927a89c1c92d92cd68a8989a66172a2303f9d604
Hossain-kobir <129597836+Hossain-kobir@users.noreply.github.com>
2025-07-25 17:21:14 +0000

    ComparisonDataTypes

---
+++ 01_Basics/ComparisonDataTypes.js
@@ -0,0 +1,23 @@
+console.log(2>2)
+console.log(2>=2)
+console.log(2==2)
+
+//num 1 to line 3 is the best comparison try to always use it
+
+console.log("2">2)
+console.log("2">=2)
+console.log("2"==2)
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined == 0)
+
+// line 7 to 17 is confusing Comparison Always avoid it 
+
+
+console.log("2"===2) 
+// line === is always strictly check the data type also 
