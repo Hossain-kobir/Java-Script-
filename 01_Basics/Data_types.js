df0cd7e1e89eed64bc4c0185459255514982c9b4
Hossain-kobir <129597836+Hossain-kobir@users.noreply.github.com>
2025-07-26 17:21:26 +0000

    Primitive & Non Primitive Data Types

--- 01_Basics/Data_types.js
+++ 01_Basics/Data_types.js
@@ -14,3 +14,37 @@ let islogin2=null //here is type of will be show object
 console.log(typeof isloggedin)
 
 console.log(typeof(islogin2))
+
+// Primitive Data Types : String , Number , Boolean , Null , Undefiend ,  symbol , Bigint
+let name ="Hossain kobir"
+
+let isloggin=true
+
+let hk = null
+
+let versity;
+
+let num=123
+
+let SymOne=Symbol("Shafayet")
+
+let Symtwo=Symbol("Shafayet")
+
+console.log(SymOne === Symtwo)
+
+const bignumber = 123344435464565465654745n
+
+// Non-Primitive Data Types : Also called Call by reference : Array , Object , Function
+
+const Movie = ["supperman","spiderman"]
+
+let myobj = {
+    Name :"Hossain Kobir",
+    Roll : 1
+}
+
+const myfun= function (){
+    print("Function is ready")
+}
+
+
