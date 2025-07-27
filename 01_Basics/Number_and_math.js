2ea7cb0ff1650f04d197064f695cb556a47aae39
Hossain-kobir <129597836+Hossain-kobir@users.noreply.github.com>
2025-07-27 12:15:21 +0000

    Number and Math in js

---
+++ 01_Basics/Number_and_math.js
@@ -0,0 +1,29 @@
+const score=1000
+
+console.log(score)
+
+const balance = new Number(55550)
+
+console.log(balance)
+
+console.log(balance.toLocaleString('bn-BD'))
+console.log(balance.toFixed(2))
+console.log(balance.valueOf())
+console.log(balance.valueOf())
+
+
+//----------------------------------Math-------------------------------
+
+console.log(Math.max(0,9,5))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.random()*max-min+1)
+
+console.log(Math.ceil(3.3))
+
+console.log(Math.floor(Math.random()*max-min+1)+min)
